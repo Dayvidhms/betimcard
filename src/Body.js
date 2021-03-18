997ee8c506ccce37dcdd00840030390483eeb689
Dayvidhms <dayvidhms@gmail.com>
2021-03-18 17:25:27 -0300

    Posicionar cartão e box cliente

--- src/Body.js
+++ src/Body.js
@@ -1,12 +1,18 @@
 import cartao from "./img/cartao.png";
+import boxcliente from "./img/box_cliente.jpg";
 
 function Body(){
     return(
         <div id="main">
-            <img src={cartao}></img>
+            <div className="grid-item">
+                <img src={cartao} alt="Área de cliente"></img>
+            </div>
 
-            <div id="boxcliente">
-                Realize a recarga do seu BetimCard, acessando um dos links abaixo, conforme o seu perfil.
+            <div className="grid-item">
+                <div id="boxcliente">
+                    <img src={boxcliente}></img>
+                    <p>Realize a recarga do seu BetimCard, acessando um dos links abaixo, conforme o seu perfil.</p>
+                </div>
             </div>
         </div>
     );
